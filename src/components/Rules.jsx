441b7a3412b3ed3b18173933845eac4f09832321
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2024-10-28 00:25:21 +0530

    Update Rules.jsx

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -5,13 +5,13 @@ const Rules = () => {
     <RulesContainer>
       <h2>How to play dice game</h2>
       <div className="text">
-        <p>Select any number</p>
+        <p>Select any Number</p>
         <p>Click on Dice Image</p>
         <p>
-          after click on dice if selected number is equal to dice number you
+          after click on dice if selected number is equal to Dice number you
           will get same point as dice{" "}
         </p>
-        <p>if you get wrong guess then 2 point will be dedcuted </p>
+        <p>if you get wrong guess then 2 point will be dedcuted. </p>
       </div>
     </RulesContainer>
   );
